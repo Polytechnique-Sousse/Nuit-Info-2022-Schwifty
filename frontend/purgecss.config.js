50c0da6cfab171232c8d2337815b505ba4e90c52
Hamza-Allani-SJ <hamza.allani.sj@gmail.com>
2022-12-02 03:30:49 +0100

    set up frontedn

---
+++ frontend/purgecss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+        '../app/templates/**/*.html',
+        '../app/templates/parts/*.html',
+        '../app/static/js/**/*.js',
+        '../app/static/js/*.js',
+    ],
+    css: [
+        '../app/static/css/**/*.css',
+        '../app/static/css/*.css'
+    ],
+    safelist: [],
+    output: "../app/static/css/"
+  }
